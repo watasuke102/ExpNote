dd11f6354c65ea9a348c0504e37d830b6319f4f8
watasuke102 <watasuke102@gmail.com>
2020-10-22 08:35:20 +0900

    Update: イベント全削除の実装

--- lib/pages/settingsPage.dart
+++ lib/pages/settingsPage.dart
@@ -23,9 +23,18 @@ class SettingsPage extends StatefulWidget
 //SettingsPageState
 class _SettingsPageState extends State<SettingsPage>
 {
-  @override
-  void initState() {
-    super.initState();
+  Widget dialog(BuildContext context)
+  {
+    return AlertDialog
+    (
+      title: Text("警告", style: Theme.of(context).textTheme.headline5),
+      content: Text("本当に全てのイベントを削除しますか？\nこの操作は取り消せません。"),
+      actions:
+      [
+        FlatButton(onPressed:(){ Navigator.pop(context,false); }, child: Text("キャンセル")),
+        FlatButton(onPressed:(){ Navigator.pop(context,true ); }, child: Text("OK", style: TextStyle(color: Colors.red),))
+      ],
+    );
   }
 
   @override Widget build(BuildContext context)
@@ -60,9 +69,37 @@ class _SettingsPageState extends State<SettingsPage>
                 Switch(value: settings.dark, onChanged: (value) =>settings.changeTheme(context)),
               ]
             ),
-            Text("${settings.dark}"),
+
+            Container(height: 80),
+
+            // データリセット
+            Row
+            (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:
+              [
+                Icon(Icons.delete, size: 40),
+                Text("データのリセット",style: Theme.of(context).textTheme.headline4)
+              ]
+            ),
+            Row
+            (
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:
+              [
+                Text("全てのイベントを削除します"),
                 FlatButton
                 (
+                  color: Colors.red[300],
+                  child: Text("実行"),
+                  onPressed:()
+                  {
+                    if(showDialog<bool>(context: context, builder: (context) => dialog(context))??false)
+                      expDataList.clearAll();
+                  }
+                ),
+              ]
+            ),
               onPressed: () {settings.changeTheme(context);},
               child: Text("change theme")
             )
